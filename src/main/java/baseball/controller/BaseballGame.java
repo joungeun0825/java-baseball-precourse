489d3a4846fcab255117eed5de2cce8e78dd1d96
joungeun0825 <joungeun0825@naver.com>
2024-04-27 00:47:20 +0900

    feat(input-utils): validate the input number

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,13 +1,22 @@
 package baseball.controller;
 
 import baseball.generator.ProgramNumGenerator;
+import utils.InputUtils;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
     public void start(){
         makeQuiz();
+        makeAnswer();
     }
     public void makeQuiz(){
         ProgramNumGenerator programNumGenerator = new ProgramNumGenerator();
         programNumGenerator.makeRandom();
+    }
+
+    public void makeAnswer(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerNumber = readLine();
     }
 }
